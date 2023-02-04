83411d6e192a1d05b1123392de56d19d8290dfe6
Annie <anniehartmann97@gmail.com>
2023-02-04 16:18:51 +0300

    Fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,18 @@ import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
 import axios from "../axios";
-import { fetchPosts } from "../redux/slices/posts";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
 
 export const Home = () => {
   const dispatch = useDispatch();
   const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status === "loading";
+  const isTagsLoading = tags.status === "loading";
 
   useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, []);
 
   return (
@@ -38,27 +40,20 @@ export const Home = () => {
               <Post
                 key={Math.random()}
                 id={1}
-                title="Roast the code #1 | Rock Paper Scissors"
+                title={obj.title}
                 imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-                user={{
-                  avatarUrl:
-                    "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-                  fullName: "Keff",
-                }}
-                createdAt={"12 июня 2022 г."}
-                viewsCount={150}
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
                 commentsCount={3}
-                tags={["react", "fun", "typescript"]}
+                tags={obj.tags}
                 isEditable
               />
             )
           )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock
-            items={["react", "typescript", "заметки"]}
-            isLoading={false}
-          />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
