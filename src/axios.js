5a82c7ca6884e2f41c2891b0985353091bd83d78
Annie <93747286+anakolchina97@users.noreply.github.com>
2024-05-08 21:08:59 +0300

    Upd axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:4444";
+
 const instance = axios.create({
-  baseURL: "https://mern-blog-back.vercel.app/",
+  baseURL: baseURL,
 });
 
 instance.interceptors.request.use((config) => {
